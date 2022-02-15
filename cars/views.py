387b2992461ebd4069258dcfe39bb4e6ad7dd8c8
rahk1990 <rahkem.ellis@gmail.com>
2022-02-14 21:46:53 -0500

    added data to admin and updated the databases carlist, added data for viewing

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,20 @@
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import CarSerializer
+from .models import Car
+
+
+
 # Create your views here.
 
 @api_view(['GET'])  
 def car_list(request):
 
+    cars = Car.objects.all() 
 
-    return Response('ok') 
+    serializers = CarSerializer(cars, many=True)
+
+    return Response(serializers.data) 
 
     
