4bba184af0f2feb0adb04cba68b7b619d9e624d5
rahk1990 <rahkem.ellis@gmail.com>
2022-02-15 13:54:52 -0500

    updated 'urls' and views '.py' improved api functions by making corrections to the remarked '.py' files.

--- cars/urls.py
+++ cars/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.car_list),
+    path('<int:pk>/', views.car_detail),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -23,6 +22,19 @@ def car_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+
+def car_detail(request, pk):
+    try:
+       car = Car.objects.get(pk=pk)
+       serializer = CarSerializer(car)
+       return Response(serializer.data)
+
+    except Car.DoesNotExist:
+        
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     # cars = Car.objects.all() 
  
